ab7a2e93ee9fe8d2b1d26bf0a1badb4bb8830474
Nelson Acosta <nelson@MacBook-Air-de-Nelson.local>
2024-05-05 22:43:47 +0200

    first

---
+++ vue-tweets/src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="image" class="profile" alt="" />
+</template>
+
+
+<script setup>
+
+import {defineProps} from 'vue'
+const props = defineProps({
+    image: String
+})
+
+
+</script>
+<style scoped></style>
---
+++ vue-tweets/src/components/Tweet.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="tweet">
+    <ProfileImage :image="tweet.user.image"></ProfileImage>
+      <div class="body">
+        <div class="top">
+          <User :user="tweet.user"></User>
+          <Timestamp :timestamp="tweet.timestamp"></Timestamp>
+        </div>
+        <Message :message="tweet.message"></Message>
+
+        <Actions></Actions>
+
+      </div>
+  
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+  </template>
+  <script setup>
+
+  import { defineProps } from 'vue';
+  import ProfileImage from './ProfileImage.vue';
+  import User from './User.vue'
+  import Timestamp from './Timestamp.vue'
+  import Message from './Message.vue'
+  import Actions from './Actions.vue'
+
+
+
+  const props = defineProps({
+        tweet: {
+          user: Object,
+          timestamp: String,
+          message: String
+        },
+  });
+
+</script>
+  
+  <style scoped>
+  a {
+    color: #42b983;
+  }
+  </style>
+  
